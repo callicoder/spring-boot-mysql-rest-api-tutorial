e2ecdd046ba3b54b9606a49b292527b24f82a915
Rajeev Kumar Singh <rajeevs@flock.co>
2018-03-04 09:25:03 +0530

    Using javax NotBlank constraint

--- src/main/java/com/example/easynotes/model/Note.java
+++ src/main/java/com/example/easynotes/model/Note.java
@@ -1,12 +1,12 @@
 package com.example.easynotes.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import org.hibernate.validator.constraints.NotBlank;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
 import java.util.Date;
 
 /**
