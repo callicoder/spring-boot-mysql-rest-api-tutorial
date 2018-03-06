3b4a873721eb87ca23743af3d392c0958f1f87fe
Rajeev Kumar Singh <rajeevs@flock.co>
2018-03-06 10:47:07 +0530

    Added @Repository annotation

--- src/main/java/com/example/easynotes/repository/NoteRepository.java
+++ src/main/java/com/example/easynotes/repository/NoteRepository.java
@@ -2,10 +2,13 @@ package com.example.easynotes.repository;
 
 import com.example.easynotes.model.Note;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 /**
  * Created by rajeevkumarsingh on 27/06/17.
  */
+
+@Repository
 public interface NoteRepository extends JpaRepository<Note, Long> {
 
 }
