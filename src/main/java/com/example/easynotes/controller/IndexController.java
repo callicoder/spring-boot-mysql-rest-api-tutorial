40bc236289635613f843f913836f16e83740788d
Rajeev Kumar Singh <rajeevs@flock.co>
2018-07-17 18:14:39 +0530

    Added an Index endpoint

---
+++ src/main/java/com/example/easynotes/controller/IndexController.java
@@ -0,0 +1,16 @@
+package com.example.easynotes.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class IndexController {
+
+    @GetMapping
+    public String sayHello() {
+        return "Hello and Welcome to the EasyNotes application. You can create a new Note by making a POST request to /api/notes endpoint.";
+    }
+}
