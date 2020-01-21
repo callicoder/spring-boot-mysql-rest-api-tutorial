072192642353e57722db71598e134371c54b68e7
Uzma Ferdaus <uferdaus@nisum.com>
2020-01-20 22:27:19 -0800

    Updated the get-method in controller

--- src/main/java/com/example/easynotes/controller/IndexController.java
+++ src/main/java/com/example/easynotes/controller/IndexController.java
@@ -13,4 +13,5 @@ public class IndexController {
     public String sayHello() {
         return "Hello and Welcome to the EasyNotes application. You can create a new Note by making a POST request to /api/notes endpoint.";
     }
+    
 }
