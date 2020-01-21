f928d74b11d9791e5dbeddf3e181c833885dc029
Uzma Ferdaus <uferdaus@nisum.com>
2020-01-20 22:35:46 -0800

    updated main class

--- src/main/java/com/example/easynotes/EasyNotesApplication.java
+++ src/main/java/com/example/easynotes/EasyNotesApplication.java
@@ -10,5 +10,6 @@ public class EasyNotesApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EasyNotesApplication.class, args);
+		System.out.println("Hello World");
 	}
 }
